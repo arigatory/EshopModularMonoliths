600a8354114467f500bb0e2a77bf743039aacdc2
Ivan Panchenko <arigatory@gmail.com>
2024-10-12 03:20:24 +0300

    wire up deps

---
+++ src/Modules/Catalog/CatalogModule.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Builder;
+namespace Catalog;
+
+public  static class CatalogModule
+{
+    public static IServiceCollection AddCatalogModule(this IServiceCollection services,
+     IConfiguration configuration)
+    {
+        return services;
+    }
+
+    public static IApplicationBuilder UseCatalogModule(this IApplicationBuilder app){
+        // app
+        //     .UseApplicationServices()
+        //     .UseInfrastructureServices()
+        //     .UseApiServices();
+        return app;
+    }
+}
---
+++ src/Modules/Ordering/OrderModule.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ordering;
+
+public static class OrderModule
+{
+    public static IServiceCollection AddOrderingModule(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        return services;
+    }
+
+
+    public static IApplicationBuilder UseOrderModule(this IApplicationBuilder app){
+        // app
+        //     .UseApplicationServices()
+        //     .UseInfrastructureServices()
+        //     .UseApiServices();
+        return app;
+    }
+}
